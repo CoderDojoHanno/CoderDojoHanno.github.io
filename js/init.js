bac4c3489eaba7db222bf0b28af1d47e381ec948
CoderDojo Hanno <coderdojohanno@gmail.com>
2017-08-20 00:50:50 +0900

    Update init.js

--- js/init.js
+++ js/init.js
@@ -4,4 +4,5 @@ $(function () {
     easing: "swing", //アニメーションイージング
     displayHeight: 0 //隠れた際に残す表示領域
   });
+  $("#homeBottom div.panel-body").eq(1).height($("#homeBottom div.panel-body").eq(0).height());
 });
